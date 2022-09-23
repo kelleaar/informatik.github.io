e350f90840a35b0b5d5a2d0b25397d5787217d9e
Aaron <114183852+kelleaar@users.noreply.github.com>
2022-09-23 09:12:03 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let zBereich;
+let version = 1;
+
+
+
+
 function setup() {
-  createCanvas(1000, 400);
+  zBereich = createCanvas(windowWidth, windowHeight);
+  zBereich.position(0,0); //Links oben
+  zBereich.style('z-index','-1') //Verschiebung auf der z-Achse
 }
 
 function draw() {
-  background(220);
+  //ohne background = transparent
+  noStroke;
+  text("Text:" + mouseX + mouseY, 50,70)
+  if(version==1) {
+  fill(mouseX/windowWidth*255,mouseY/windowWidth*255,0);
+  circle(mouseX, mouseY, 20);
+  }
+  if(version==2) {
+    if(mouseX >= windowWidth/2) {
+      fill(255,0,0);
+      circle(mouseX, mouseY, 20);
+    } else{
+      fill(0,0,255);
+    circle(mouseX, mouseY, 20);
+    }
+  }
 }
